9a81f1c8152cc7f43700e891d9f9b783e882cbff
Ivan <ivanmajer011@gmail.com>
2026-02-25 18:19:27 +0100

    izvlacenje 1 signala i prikaz

--- nm.py
+++ nm.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy.signal import butter, filtfilt
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def banpassFilter(signal, lowcut=20, highcut=450, fs=2000, order=4):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return filtfilt(b, a, signal, axis=0)
+
+# primer: raw_signal.shape = (uzorci, kanali)
 
 '''
 datasetovi su oblika 8x8senzora + labela (klasa e [0,3])
@@ -26,9 +37,29 @@ izlaz3 = data3.iloc[:, -1].values
 
 svi_izlazi = np.concatenate([izlaz0, izlaz1, izlaz2, izlaz3])
 
+'''
 plt.figure()
 plt.hist(svi_izlazi, bins=20, color='skyblue', edgecolor='black')
 plt.xlabel("Vrednost izlaza")
 plt.ylabel("Broj uzoraka")
 plt.title("Histogram izlaza po svih 4 dataset-a")
 plt.show()
+'''
+signal = data0.iloc[:500, :-1].values
+labels = data0.iloc[:500, -1].values 
+
+# signal.shape = (500, 64) → 500 uzoraka, 64 kanala
+fs = 200  # Hz
+dt = 1/fs  # 0.05
+t = np.arange(0, signal.shape[0]*dt, dt)
+plt.figure(figsize=(10,4))
+plt.plot(t, signal[:,0])
+plt.title("EMG kanal 0 - 20s signal")
+plt.xlabel("Vreme [s]")
+plt.ylabel("Amplituda")
+plt.show()
+
+filtered_signal = banpassFilter(signal)
+
+# Koristiti sliding window??
+# za CNN dimenzije(8, 8, 1)
