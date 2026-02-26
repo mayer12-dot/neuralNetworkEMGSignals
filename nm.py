c36a4f3c7b5f2a99ccdb884679c387ca44cc5721
Ivan <ivanmajer011@gmail.com>
2026-02-26 20:27:19 +0100

    prikaz jednog reda

--- nm.py
+++ nm.py
@@ -23,12 +23,12 @@ data1 = pd.read_csv('emgSignaliKlasa1.CSV', header=None)
 data2 = pd.read_csv('emgSignaliKlasa2.CSV', header=None)
 data3 = pd.read_csv('emgSignaliKlasa3.CSV', header=None)
 
-'''
+
 print(data0.shape)
 print(data1.shape)
 print(data2.shape)
 print(data3.shape)
-'''
+
 
 izlaz0 = data0.iloc[:, -1].values
 izlaz1 = data1.iloc[:, -1].values
@@ -45,21 +45,35 @@ plt.ylabel("Broj uzoraka")
 plt.title("Histogram izlaza po svih 4 dataset-a")
 plt.show()
 '''
-signal = data0.iloc[:500, :-1].values
-labels = data0.iloc[:500, -1].values 
 
-# signal.shape = (500, 64) → 500 uzoraka, 64 kanala
-fs = 200  # Hz
-dt = 1/fs  # 0.05
-t = np.arange(0, signal.shape[0]*dt, dt)
-plt.figure(figsize=(10,4))
-plt.plot(t, signal[:,0])
-plt.title("EMG kanal 0 - 20s signal")
+#prikaz prvih 8 uzastopnih trenutaka, umesto K ubaciti klasu
+'''
+signal = data3.iloc[0, :-1].values
+#label = data1.iloc[0, -1]
+
+signal_reshaped = signal.reshape(8, 8)  # (time, sensors)
+
+fs = 200
+dt = 1/fs  # 0.005 s
+
+t = np.arange(8) * dt   # samo 8 vremenskih tačaka
+
+plt.figure(figsize=(8,4))
+plt.plot(t, signal_reshaped[:, 0])  # senzor 0 kroz 8 trenutaka
+plt.title("EMG senzor 0 - 40ms segment")
 plt.xlabel("Vreme [s]")
 plt.ylabel("Amplituda")
 plt.show()
 
-filtered_signal = banpassFilter(signal)
+plt.figure(figsize=(10,6))
+for i in range(8):
+    plt.plot(t, signal_reshaped[:, i], label=f"Senzor {i}")
 
-# Koristiti sliding window??
+plt.xlabel("Vreme [s]")
+plt.ylabel("Amplituda")
+plt.title("8 senzora kroz 8 uzastopnih trenutaka (40ms)")
+plt.legend()
+plt.show()
+''' 
+
 # za CNN dimenzije(8, 8, 1)
